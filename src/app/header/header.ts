b24ea0c19c49584715204ecc9a80e3e741402d5e
otogelashvili <otarbasilevsi@gmail.com>
2025-12-13 07:42:17 +0400

    feat: add navbar logic

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,20 +1,28 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, inject, ViewChild } from '@angular/core';
 import { NgStyle } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { NavigationEnd, Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { filter } from 'rxjs';
+
 @Component({
   selector: 'app-header',
-  imports: [NgStyle, RouterLink],
+  imports: [NgStyle, RouterLink, RouterLinkActive],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header implements AfterViewInit {
   @ViewChild('nav', { static: true }) navRef!: ElementRef<HTMLElement>;
+
   bgElPosition = 0;
   bgElWidth = 0;
   private navRect!: DOMRect;
+  private router = inject(Router);
 
   ngAfterViewInit() {
     this.navRect = this.navRef.nativeElement.getBoundingClientRect();
+
+    this.router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe(() => {
+      this.resetBg();
+    });
   }
 
   moveBg(event: MouseEvent) {
@@ -26,6 +34,17 @@ export class Header implements AfterViewInit {
   }
 
   resetBg() {
-    this.bgElPosition = 0;
+    const currentPath = this.router.url.slice(1);
+    let activeLink: HTMLElement | null;
+    if (currentPath !== '') {
+      activeLink = this.navRef.nativeElement.querySelector(`#${currentPath}`);
+    } else {
+      activeLink = this.navRef.nativeElement.querySelector('#sessions');
+    }
+    if (activeLink) {
+      const activeLinkRect = activeLink.getBoundingClientRect();
+      this.bgElPosition = activeLinkRect.left - this.navRect.left;
+      this.bgElWidth = activeLinkRect.width;
+    }
   }
 }
