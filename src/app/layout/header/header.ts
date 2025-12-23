4ddb1a8bcf2d7ba2d23a320ba77d629d47b4b4b8
otogelashvili <otarbasilevsi@gmail.com>
2025-12-23 15:09:45 +0400

    refactor: move heading to navigations component for local state

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,24 +1,17 @@
-import { Component, LOCALE_ID, ViewChild, inject, signal } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, LOCALE_ID, inject, signal } from '@angular/core';
 import { Loader } from '../../components/loader/loader';
 import { ProfileBtn } from './components/nav-menu/nav-menu';
 import { Navigation } from './components/navigation/navigation';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, Loader, ProfileBtn, Navigation],
+  imports: [Loader, ProfileBtn, Navigation],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
-  @ViewChild(Navigation) navigationComponent!: Navigation;
   private currentLocale = inject(LOCALE_ID);
   loading = signal(false);
-
-  logoReset = () => {
-    // Calls method from the Navigation component
-    this.navigationComponent?.logoReset();
-  };
 
   switchLang() {
     const pathname = window.location.pathname;
