6689137b44f88b51cc7def65f266b8549c6d3f99
otogelashvili <otarbasilevsi@gmail.com>
2025-12-23 19:40:50 +0400

    chore: remove unused imports

--- src/app/layout/header/components/navigation/navigation.ts
+++ src/app/layout/header/components/navigation/navigation.ts
@@ -1,5 +1,14 @@
 import { NgStyle } from '@angular/common';
-import { AfterViewInit, Component, ElementRef, OnDestroy, signal, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  DOCUMENT,
+  ElementRef,
+  inject,
+  OnDestroy,
+  signal,
+  ViewChild,
+} from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
@@ -10,21 +19,25 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
 })
 export class Navigation implements AfterViewInit, OnDestroy {
   @ViewChild('nav', { static: false }) navRef!: ElementRef<HTMLElement>;
-  bgElPosition = signal(0);
-  bgElWidth = signal(0);
+  readonly bgElPosition = signal(0);
+  readonly bgElWidth = signal(0);
   private navRect!: DOMRect;
+  private document = inject(DOCUMENT);
 
   ngAfterViewInit() {
-    // so what actually happes here is that yes DOM is ready, but external assets like ( styles, images, font families...) are not, in some cases accurately calculated
-    // therefore we use load event and calculate rect after  all assets load
-    // using router event to subsbscribe to instance of navigationEnd event also had a weird bug ,even when bgReset() was run with setTimout, on initial load navRect would have wrong value.
-    // cause of bug is external asset loading, which can probably be fixed by prefetching or self hosting those assets
-    window.addEventListener('load', this.initialSetup);
+    // so what actually happes here is that yes DOM is ready, but external assets like ( styles, images, font families...) are not
+    // therefore we use API to run function afrer fonts are loaded
+
+    this.document.fonts.ready.then(() => {
+      this.initialSetup();
+
+      //we listen to resize event so navRect gets coreccly recalculated on screen size change
+      window.addEventListener('resize', this.initialSetup);
+    });
   }
   ngOnDestroy() {
-    window.removeEventListener('load', this.initialSetup);
+    window.removeEventListener('resize', this.initialSetup);
   }
-
   // lexicly binds "this" to header instance so we dont have to use explicit binding inside events
   private initialSetup = () => {
     this.navRect = this.navRef.nativeElement.getBoundingClientRect();
@@ -46,7 +59,7 @@ export class Navigation implements AfterViewInit, OnDestroy {
     this.bgElWidth.set(activeLinkRect.width);
   };
   logoReset = () => {
-    const homeLink = document.querySelector<HTMLElement>('.homeLink');
+    const homeLink = this.document.querySelector<HTMLElement>('.homeLink');
     if (homeLink) {
       const heomLinkWidth = homeLink.getBoundingClientRect().width;
       this.bgElPosition.set(0);
