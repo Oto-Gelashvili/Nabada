79259a844c97eae0e2cc07b9ce24b14c87e30ba0
otogelashvili <otarbasilevsi@gmail.com>
2026-01-24 10:43:22 +0400

    feat: add sing up page

--- src/app/features/auth/pages/sing-up/sing-up.ts
+++ src/app/features/auth/pages/sing-up/sing-up.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-sing-up',
-  imports: [],
+  imports: [RouterLink, FormsModule],
   templateUrl: './sing-up.html',
   styleUrl: './sing-up.css',
 })
 export class SingUp {
-
+  email = signal('');
+  isShaking = signal(false);
+  submit(form: NgForm) {
+    if (form.invalid) {
+      this.triggerShake();
+    } else {
+      console.log('Form Submitted!', this.email());
+    }
+  }
+  triggerShake() {
+    this.isShaking.set(true);
+    setTimeout(() => {
+      this.isShaking.set(false);
+    }, 400);
+  }
 }
