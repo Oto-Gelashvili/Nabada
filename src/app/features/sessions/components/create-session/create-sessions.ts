5b89db82419959948f040c185147222c7df72714
otogelashvili <otarbasilevsi@gmail.com>
2026-02-17 00:13:39 +0400

    feat: improve UI + add template card to createSessions

--- src/app/features/sessions/components/create-session/create-sessions.ts
+++ src/app/features/sessions/components/create-session/create-sessions.ts
@@ -9,17 +9,18 @@ import {
 import { DatePickerModule } from 'primeng/datepicker';
 import { CheckboxModule } from 'primeng/checkbox';
 import { Station } from '../../../../models/sessions';
-import { Product } from '../../../../models/products.model';
-
+import { Product, ProductAmount } from '../../../../models/products.model';
+import { CurrencyPipe } from '@angular/common';
 @Component({
   selector: 'app-create-session',
-  imports: [ReactiveFormsModule, CheckboxModule, DatePickerModule, FormsModule],
+  imports: [ReactiveFormsModule, CheckboxModule, DatePickerModule, FormsModule, CurrencyPipe],
   templateUrl: './create-sessions.html',
   styleUrls: ['./create-sessions.css'],
 })
 export class CreateSessionComponent implements OnInit {
   stations = input<Station[]>([]);
   products = input<Product[]>([]);
+  amounts = signal<ProductAmount[]>([]);
   isCustomSelectOpen = signal(false);
   isCustomMultiSelectOpen = signal(false);
   close = output<void>();
@@ -71,8 +72,10 @@ export class CreateSessionComponent implements OnInit {
     let newIds: number[];
     if (index > -1) {
       newIds = currentIds.filter((id) => id !== product.id);
+      this.amounts.update((prev) => prev.filter((a) => a.id !== product.id));
     } else {
       newIds = [...currentIds, product.id];
+      this.amounts.update((prev) => [...prev, { id: product.id, amount: 1 }]);
     }
 
     this.createSessionForm.patchValue({ productIds: newIds });
@@ -85,10 +88,43 @@ export class CreateSessionComponent implements OnInit {
   removeProduct(productId: number) {
     const currentIds = this.createSessionForm.controls.productIds.value;
     const newIds = currentIds.filter((id) => id !== productId);
-
     this.createSessionForm.patchValue({ productIds: newIds });
   }
+  getAmount(productId: number): number {
+    const item = this.amounts().find((a) => a.id === productId);
+    return item ? item.amount : 1;
+  }
+  getTotalPrice(product: Product): number {
+    const amount = this.getAmount(product.id);
+    return product.price * amount;
+  }
 
+  addAmount(productId: number) {
+    this.amounts.update((current) => {
+      const index = current.findIndex((item) => item.id === productId);
+
+      if (index === -1) {
+        return [...current, { id: productId, amount: 2 }];
+      }
+
+      const updated = [...current];
+      updated[index] = { ...updated[index], amount: updated[index].amount + 1 };
+      return updated;
+    });
+  }
+  removeAmount(productId: number) {
+    this.amounts.update((current) => {
+      const index = current.findIndex((item) => item.id === productId);
+      if (current[index].amount === 1) {
+        this.removeProduct(productId);
+        return current;
+      }
+
+      const updated = [...current];
+      updated[index] = { ...updated[index], amount: current[index].amount - 1 };
+      return updated;
+    });
+  }
   onSubmit() {
     console.log(this.createSessionForm.value);
   }
--- src/app/models/products.model.ts
+++ src/app/models/products.model.ts
@@ -3,3 +3,7 @@ export interface Product {
   name: string;
   price: number;
 }
+export interface ProductAmount {
+  id: number;
+  amount: number;
+}
