e7eddc02364d7978f29cafbad651b306082e14fd
otogelashvili <otarbasilevsi@gmail.com>
2026-02-27 13:05:01 +0400

    feat: add primeNg date pickers with custom styles

--- src/app/features/analytics/analytics.ts
+++ src/app/features/analytics/analytics.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
-import { LineGraphComponent } from './components/line-graph';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { DatePickerModule } from 'primeng/datepicker';
 import { GraphPoint } from './models/analytics.models';
+import { LineGraphComponent } from './components/line-graph';
 
 @Component({
   selector: 'app-analytics',
-  imports: [LineGraphComponent],
+  imports: [LineGraphComponent, DatePickerModule, FormsModule],
   templateUrl: './analytics.html',
   styleUrl: './analytics.css',
 })
 export class Analytics {
+  readonly today = new Date();
+
+  readonly startDate = signal<Date>(this.getDefaultStart());
+  readonly endDate = signal<Date>(new Date());
+
   readonly dataPoints: GraphPoint[] = [
     { date: 'Mon', total: 40 },
     { date: 'Tue', total: 80 },
@@ -18,4 +25,14 @@ export class Analytics {
     { date: 'Sat', total: 110 },
     { date: 'Sun', total: 75 },
   ];
+
+  onDateChange() {
+    // later: reload data from Supabase
+  }
+
+  private getDefaultStart(): Date {
+    const d = new Date();
+    d.setDate(d.getDate() - 7);
+    return d;
+  }
 }
