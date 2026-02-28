e6fe78d61826da45dabfe5b385743f7a6e97ee89
otogelashvili <otarbasilevsi@gmail.com>
2026-02-28 08:15:55 +0400

    feat: replace native datepicker with custom made

--- src/app/features/sessions/components/sessions-header/sessions-header.ts
+++ src/app/features/sessions/components/sessions-header/sessions-header.ts
@@ -1,10 +1,11 @@
-import { Component, computed, ElementRef, input, model, output, ViewChild } from '@angular/core';
-import { DatePipe } from '@angular/common';
+import { Component, computed, input, model, output } from '@angular/core';
+import { DatePickerModule } from 'primeng/datepicker';
+import { FormsModule } from '@angular/forms';
 import { DateUtils } from '../../../../shared/components/utils/date.utils';
 
 @Component({
   selector: 'app-sessions-header',
-  imports: [DatePipe],
+  imports: [DatePickerModule, FormsModule],
   templateUrl: './sessions-header.html',
   styleUrls: ['./sessions-header.css'],
 })
@@ -15,41 +16,17 @@ export class SessionsHeaderComponent {
   cancelEdit = output<void>();
   createClick = output<void>();
 
-  @ViewChild('datePickerInput') datePickerInput!: ElementRef<HTMLInputElement>;
-  private readonly today = new Date();
-
-  dateInputValue = computed(() => DateUtils.toISODate(this.date()));
-
-  maxDate = computed(() => DateUtils.toISODate(this.today));
-
-  isToday = computed(() => DateUtils.isToday(this.date()));
+  readonly today = new Date();
+  readonly isToday = computed(() => DateUtils.isToday(this.date()));
 
   changeDay(days: number) {
     const newDate = new Date(this.date());
     newDate.setDate(newDate.getDate() + days);
-
     if (newDate > this.today) return;
-
     this.date.set(newDate);
   }
 
   onCancel() {
     this.cancelEdit.emit();
-  }
-
-  openDatePicker() {
-    try {
-      this.datePickerInput.nativeElement.showPicker();
-    } catch {
-      this.datePickerInput.nativeElement.click();
-    }
-  }
-
-  onDatePicked(event: Event) {
-    const input = event.target as HTMLInputElement;
-    if (!input.value) return;
-
-    const [year, month, day] = input.value.split('-').map(Number);
-    this.date.set(new Date(year, month - 1, day));
   }
 }
