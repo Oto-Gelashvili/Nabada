15cd9d81eee143c320c39b71132bdd33ea3a7df3
otogelashvili <otarbasilevsi@gmail.com>
2026-03-02 14:09:24 +0400

    feat: add public header

--- src/app/layouts/public-layout/public-layout.ts
+++ src/app/layouts/public-layout/public-layout.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from './components/header/header';
+import { Header } from '../auth-layout/components/header/header';
+// import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-public-layout',
